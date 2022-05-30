8b47b861eacda131e4b6e037cbad4612a6a76d52
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-05-30 15:37:23 -0300

    yeaah

---
+++ exercicio4/index.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const pcInfo = require('./os/os')
+const server = http.createServer((request, response) => {
+    const {
+        url,
+        method
+    } = request
+    if (method !== 'GET') {
+        response.writeHead(405)
+        response.end(JSON.stringify({
+            message: 'Method not allowed'
+        }))
+    }
+    if (url === '/pcinfo') {
+        response.writeHead(200)
+        pcInfo()
+    }
+})
+
+
+
+server.listen(3000, () => {
+    console.log("Server listening on port 3000")
+})
